b6f52c86e48b9c39ce3e62740201e3c74f440401
LIGHT131313 <126007478+LIGHT131313@users.noreply.github.com>
2024-03-10 21:34:14 +0000

    Deploying to gh-pages from @ LIGHT131313/Psychologists-Services@370c0297ce23cc96f95a93b106d6df5d7a391b15 🚀

--- static/js/592.587d386a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpsychologists_services=self.webpackChunkpsychologists_services||[]).push([[592],{592:(s,e,c)=>{c.r(e),c.d(e,{default:()=>n});var r=c(781),i=c(579);const t=()=>(0,i.jsx)(r.m,{children:(0,i.jsx)("p",{children:"Favorites"})});function n(){return(0,i.jsx)(t,{})}}}]);
-//# sourceMappingURL=592.587d386a.chunk.js.map
